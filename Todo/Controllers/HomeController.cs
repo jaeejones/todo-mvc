5a93cd4ce7adb4804e369f27527c951d174b873c
Jael <jaele.jones@gmail.com>
2016-07-19 20:17:35 -0400

    Changed Name of Application

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Todo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Tasks to Accomplish.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "We can code it student.";
 
             return View();
         }
